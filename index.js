e52f1bac2d7504e2de9d6e3eb509ed8136083b36
Moshe <moshe.oz@ust-global.com>
2020-09-24 18:43:09 +0300

    indentation

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require("express");
+const express = require('express'); 
 const app = express();
-
 app.use(express.static('public'));
 app.get('/', (req, res) => {
      res.sendFile('index.html', {root: __dirname + '/public/'});
